f0ab9f74717215b50c58717cf3f0f53afb3eb098
Ayushi Jain <ayushij@cisco.com>
2017-01-23 15:43:12 -0500

    NPM - Understanding node package manager

---
+++ Chapter2/app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var port = 3000;
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+  var segments = url.parse(req.url, true).pathname;
+  segments = segments.split('/').splice(1);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (segments.length > 0 && segments[0] === 'greet') {
+    var msg = 'Hey there, ';
+    if (segments.length > 1) {
+      msg += segments[1] + '! ';
+    }
+    res.write(msg + 'Welcome to your first Node.js project');
+  } else {
+    res.write('Hello World');
+  }
+  res.end();
+});
+
+server.listen(port);
+console.log('Server listening at http://localhost:' + port);
